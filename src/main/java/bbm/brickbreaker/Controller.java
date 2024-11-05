ce1602758f8aa6e28e96eb9a15ddca30bf3ba53d
Aliza Buchen <alizabuchen@alizas-air-2.mynetworksettings.com>
2024-11-05 11:13:10 -0500

    controller

--- src/main/java/bbm/brickbreaker/Controller.java
+++ src/main/java/bbm/brickbreaker/Controller.java
@@ -1,4 +1,55 @@
 package bbm.brickbreaker;
 
-public class Controller {
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameController {
+    private final Ball ball;
+    private final Brick brick;
+    private final Panel panel;
+
+    private Timer timer;
+
+    public GameController(Ball ball, Brick brick, Panel panel) {
+        this.ball = ball;
+        this.brick = brick;
+        this.panel = panel;
+        // do we want the gameWidth and gameHeight?
+        timer = new Timer(1000, e -> {
+
+        });
+
+    }
+
+    public void startTimer() {
+        timer.start();
+    }
+
+    private void breakBricks(int x, int y) {
+
+        //need logic of how to determine if brick is hit - does this happen in controller?
+
+        brick.hitBrick(x, y);
+
+    }
+
+    private void hitWall() {
+        double nextX = ball.locationX();
+        double nextY = ball.locationY();
+        if (nextX <= 0 || nextX >= gameWidth) {
+            ball.bounce();
+        }
+        if (nextY <= 0) {
+            ball.bounce(); // Bounce vertically
+        }
+    }
+
+    // Stops the game if the ball goes below the panel
+    private void gameOver() {
+        if (ball.getY() > gameHeight) {
+            timer.stop();
+            System.out.println("Game Over!");
+        }
+    }
 }
