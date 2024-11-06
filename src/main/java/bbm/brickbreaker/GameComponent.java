efc61b79e726a89af48a7705dfa0b2d93b931b45
FIRST_NAME LAST_NAME <cblitz2@student.touro.edu>
2024-11-05 22:21:50 -0500

    created view

--- src/main/java/bbm/brickbreaker/GameComponent.java
+++ src/main/java/bbm/brickbreaker/GameComponent.java
@@ -4,24 +4,40 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameComponent extends JComponent {
-    Brick brick;
+    private final Brick brick;
+    private final Ball ball;
 
-    public GameComponent(Brick brick) {
+    private final Panel panel;
+
+    public GameComponent(Brick brick, Ball ball, Panel panel) {
         this.brick = brick;
+        this.ball = ball;
+        this.panel = panel;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.RED);
-        for (int i = 0; i < brick.getWidth(); i++) {
-            for (int j = 0; j < brick.getHeight(); j++) {
+        for (int i = 0; i < brick.getCols(); i++) {
+            for (int j = 0; j < brick.getRows(); j++) {
                 if (brick.isBrick(i, j)) {
-                    g.fillRect(i, j, 25, 15);
+                    g.setColor(Color.RED);
+                    int xPos = i * 30;
+                    int yPos = j * 18;
+                    g.fillRect(xPos, yPos, 30, 18);
+
+                    g.setColor(Color.BLACK);
+                    g.drawRect(xPos, yPos, 30, 18);
                 }
             }
         }
-        //g.setColor(Color.BLUE);
-       // g.fillRect(getWidth() / 2 - 15, getHeight() - 30, 40, 10);
+
+        g.setColor(Color.DARK_GRAY);
+        g.fillOval((int)ball.getX() - 10, (int)ball.getY()  - 10, 20, 20); // Draw ball at its current position
+
+
+        g.setColor(Color.BLUE);
+        g.fillRect(panel.getX(),  panel.getY(), panel.getWidth(), panel.getHeight());
+
     }
 }
--- src/main/java/bbm/brickbreaker/GameFrame.java
+++ src/main/java/bbm/brickbreaker/GameFrame.java
@@ -1,100 +1,3 @@
-//package bbm.brickbreaker;
-//
-//import javax.swing.*;
-//import java.awt.*;
-//import java.awt.event.MouseAdapter;
-//import java.awt.event.MouseEvent;
-//import java.awt.event.MouseMotionAdapter;
-//
-//import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseListener;
-//import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseMotionListener;
-//
-//public class GameFrame extends JFrame {
-//
-//    public GameFrame() {
-//        setSize(800, 600);
-//        setTitle("Brick Breaker");
-//        setDefaultCloseOperation(EXIT_ON_CLOSE);
-//
-//        Brick brick = new Brick(500, 200);
-//        brick.populateBricks();
-//
-//        GameComponent component = new GameComponent(brick);
-//        setLayout(new BorderLayout());
-//        add(component, BorderLayout.CENTER);
-//
-//        JLabel bar = new JLabel();
-//
-////
-////
-////        //Panel panel = new Panel();
-////        //panel.movePanel();
-////        int offsetX;
-////        int offsetY;
-////        boolean isDragging = false;
-////
-////        bar.addMouseMotionListener(new MouseMotionAdapter() {
-////            @Override
-////            public void mouseDragged(MouseEvent e) {
-////                panel.movePanel(e, isDragging, offsetX, offsetY);
-//////                if (isDragging) {
-//////                    // Calculate the new position of the panel
-//////                    int currentX = e.getXOnScreen(); // Get mouse position relative to screen
-//////                    int currentY = e.getYOnScreen();
-//////
-//////                    // Set the new location of the panel
-//////                    setLocation(currentX - offsetX, currentY - offsetY);
-//////                }
-////            }
-////        });
-////
-////        bar.addMouseListener(new MouseAdapter() {
-////
-////            @Override
-////            public void mousePressed(MouseEvent e) {
-////                // Store the initial position of the mouse relative to the panel
-////                e.getX();
-////                e.getY();
-////                isDragging = true; // Set dragging flag
-////            }
-////
-////            @Override
-////            public void mouseReleased(MouseEvent e) {
-////                isDragging = false; // Reset dragging flag
-////            }
-////        });
-////    }
-//
-//        // Offset variables to track drag offset
-//        final int[] offsetX = {0};
-//        final int[] offsetY = {0};
-//
-//        // Mouse listeners for dragging
-//        bar.addMouseListener(new MouseAdapter() {
-//            @Override
-//            public void mousePressed(MouseEvent e) {
-//                // Capture the offset at the start of the drag
-//                offsetX[0] = e.getX();
-//                offsetY[0] = e.getY();
-//            }
-//        });
-//
-//        bar.addMouseMotionListener(new MouseMotionAdapter() {
-//            @Override
-//            public void mouseDragged(MouseEvent e) {
-//                // Calculate new position based on mouse location and offset
-//                int newX = e.getXOnScreen() - offsetX[0];
-//                int newY = e.getYOnScreen() - offsetY[0];
-//
-//                // Set the new location of the JLabel
-//                bar.setLocation(newX, newY);
-//            }
-//        });
-//        add(bar, BorderLayout.SOUTH);
-//    }
-//}
-
-
 package bbm.brickbreaker;
 
 import javax.swing.*;
@@ -109,29 +12,35 @@ public class GameFrame extends JFrame {
         setSize(800, 600);
         setTitle("Brick Breaker");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(null); // Use null layout for absolute positioning
+        setLayout(null);
 
-        // Create and populate bricks
+        Panel panel = new Panel(350, 520);
+        Ball ball = new Ball(45, 800, 600, panel.getX() + 40, panel.getY() - 20); // Adjust angle, width, and height as needed
         Brick brick = new Brick(500, 200);
         brick.populateBricks();
 
-        // Add the game component for bricks
-        GameComponent component = new GameComponent(brick);
-        component.setBounds(0, 0, 800, 600); // Full size for component
+        GameComponent component = new GameComponent(brick, ball, panel);
+        component.setBounds(0, 0, 800, 600);
         add(component);
 
-        // Create and configure the bar
+        Controller controller = new Controller(ball, brick, 10, panel, component);
+
         JLabel bar = new JLabel();
         bar.setOpaque(true);
         bar.setBackground(Color.BLUE);
-        bar.setBounds(350, 520, 100, 20); // Position bar near the bottom of the screen
+        bar.setBounds(panel.getX(), panel.getY(), panel.getWidth(), panel.getHeight());
         add(bar);
 
-        // Offset variables for tracking drag
         int[] offsetX = {0};
         int[] offsetY = {0};
 
-        // Mouse listeners for dragging the bar
+        bar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                controller.play();
+            }
+        });
+
         bar.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
@@ -144,8 +53,9 @@ public class GameFrame extends JFrame {
             @Override
             public void mouseDragged(MouseEvent e) {
                 int newX = e.getXOnScreen() - offsetX[0];
-                int newY = bar.getY(); // Lock the Y position so it only moves horizontally
+                int newY = bar.getY();
                 bar.setLocation(newX, newY);
+                panel.setLocation(newX, newY);
             }
         });
 
